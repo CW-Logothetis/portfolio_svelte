2bdda54eabbb15592a4a74be8bc4bf1d427a9ecd
CW-Logothetis <cwlogo@pm.me>
2023-02-05 21:04:24 +0200

    adding more global styles, colors

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>    
+    import '../global.scss'
+    import Footer from "$lib/components/Footer/Footer.svelte";
+	import NavBar from '$lib/components/NavBar/NavBar.svelte';
+
+</script> 
+    
+<NavBar />
+
+<main class="main-grid">
+    <slot />
+</main>
+
+<Footer />
+
+<style lang="scss">
+
+</style>
