5d8832d3651da5a377299abfa7b41fb63aa2a61b
CW-Logothetis <chris.logothetis@cognitivplus.com>
2023-11-15 16:31:27 +0200

    re-usable Intro comp w/ named slots

--- src/lib/components/ancient-theatres/Intro.svelte
+++ src/lib/components/ancient-theatres/Intro.svelte
@@ -1,19 +1,24 @@
 <section id="introduction" class="intro">
-           
-    <div class="main-grid">
-        <h1 class="intro--title">Ancient Greek Theatres</h1>
-        <p class="intro--para">As I’m a bit of a history nut, I’ve visited quite a few of the archaeological sites in Greece and the southern Balkans. So when I was thinking of a personal project to test my Figma and responsive design skills, I thought this would be a good fit. Especially as I have my own photographs to use.</p>            
-        <p class="intro--para">There are over a hundred Greek and then Roman theatres, so I just selected five from the Greek period, but nipped over the border into modern day Albania to show two that are relatively unknown.</p>            
-        <p class="intro--para">Note: as the aim here was to practice web design, the theatre pages use placeholder or duplicated text.</p>
-    </div>
 
-    <div class="intro--stack main-grid">
-        <p class="intro--para-col-1"><strong>Stack:</strong></p>
-        <p class="intro--para-col-2">Vanilla JS - CSS - Git/Github</p>
-        <p class="intro--para-col-1"><strong>Design:</strong></p>
-        <p class="intro--para-col-2">Figma - VS Code - Chrome Dev Tools</p>
-        <p class="intro--para-col-1"><strong>Live Site:</strong></p>
-        <p class="intro--para-col-2"><a href="https://ancient-theatres.netlify.app/" class="intro--para-col-2" target="_blank" rel="noreferrer">Netlify</a></p>
+  <div class="center" style="--center-max-inline-size: 70ch">
+    <h1 class="intro--title">
+      <slot name="heading">
+        <span class="missing">Missing h1</span>
+      </slot>
+    </h1>
+    <div class="intro--para">
+      <slot name="intro-para">
+        <span class="missing">Missing intro p</span>
+      </slot>
     </div>
+  </div>
+
+  <div
+    class="intro-container center"
+    style="--center-max-inline-size: 70ch"
+  >
+    <slot name="tech-stack"> <span class="missing">Missing tech</span></slot>
+  </div>
+
 
 </section>
--- src/routes/ancient-theatres/+page.svelte
+++ src/routes/ancient-theatres/+page.svelte
@@ -5,10 +5,40 @@
   import Process from "$lib/components/ancient-theatres/Process.svelte";
   import NextSteps from "$lib/components/ancient-theatres/NextSteps.svelte";
   import "$lib/scss/global.scss";
+
+  let techStack = [
+    { title: 'Stack:', description: 'Vanilla JS - CSS - Git/Github' },
+    { title: 'Design:', description: 'Figma - VS Code - Chrome Dev Tools' },
+    { title: 'Demo:', description: '<a href="https://ancient-theatres.netlify.app/" class="intro--para-col-2" target="_blank" rel="noreferrer">Netlify</a>' }
+  ];
 </script>
 
 <div>
-  <Intro />
+  <Intro>
+    <span slot="heading">Ancient Greek Theatres</span>
+    <div slot="intro-para">
+      <p>As I’m a bit of a history nut, I’ve visited quite a few of the archaeological sites in Greece and the southern Balkans. So when I was thinking of a personal project to test my Figma and responsive design skills, I thought this would be a good fit. Especially as I have my own photographs to use.</p>
+      <p>There are over a hundred Greek and then Roman theatres, so I just selected five from the Greek period, but nipped over the border into modern day Albania to show two that are relatively unknown.</p>
+      <p>Note: as the aim here was to practice web design, the theatre pages use placeholder or duplicated text.</p>
+    </div>
+
+    <div slot="tech-stack">
+      {#each techStack as { title, description }}
+        <div class="line">
+          <div class="title">{title}</div>
+          <div class="description">{@html description}</div>
+        </div>
+      {/each}
+    </div>
+
+    <div slot="descriptions">
+
+
+    </div>
+
+
+  </Intro>
+
   <Hero />
   <Process />
   <Bugs />
