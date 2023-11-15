d74968bddb652d5aaa15d3d37c290def081aba46
CW-Logothetis <chris.logothetis@cognitivplus.com>
2023-11-15 20:08:57 +0200

    refactor Hero css

--- src/lib/components/ancient-theatres/Hero.svelte
+++ src/lib/components/ancient-theatres/Hero.svelte
@@ -1,41 +1,14 @@
-<section class="main-grid">
+<section class="center">
     <img src="/theatres-hero.png" class="project-hero" alt="">
 </section>
 
 <style lang="scss">
-
 .project-hero {
-    grid-column: 2/-2;
-    width: 100%;
+    //width: 100%;
     max-width: 1200px;
-    margin: 2em auto 7em auto;
+    max-height: 90dvh;
+    margin-inline: auto;
     border-radius: 7px;
     box-shadow: 6.7px 13.4px 13.4px hsl(0deg 0% 0% / 0.29);
 }
-
-@media (min-width: 400px) {
-    .project-hero {
-        margin: 0 auto 7em auto;
-    }
-}
-
-@media (min-width: 700px) {
-    .project-hero {
-        margin: 0 auto 11em auto;
-    }
-}
-
-@media (min-width: 900px) {
-    .project-hero {
-        margin: 5em auto 12em auto;
-    }
-}
-
-@media (min-width: 1050px) {
-    .project-hero {
-        grid-column: 1/-1;
-        margin: 0 auto 20em auto;
-    }
-}
-
 </style>
