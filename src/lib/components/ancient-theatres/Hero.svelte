f59c3514507e8d956da4cd3fd93fe3a02460c34a
CW-Logothetis <chris.logothetis@cognitivplus.com>
2023-11-15 20:31:19 +0200

    fix resposive hero

--- src/lib/components/ancient-theatres/Hero.svelte
+++ src/lib/components/ancient-theatres/Hero.svelte
@@ -4,9 +4,8 @@
 
 <style lang="scss">
 .project-hero {
-    //width: 100%;
-    max-width: 1200px;
-    max-height: 90dvh;
+    width: 100%;
+    max-width: 1000px;
     margin-inline: auto;
     border-radius: 7px;
     box-shadow: 6.7px 13.4px 13.4px hsl(0deg 0% 0% / 0.29);
