1a99ae31b33046c7b8a784a048a47c6ce4695b17
CW-Logothetis <chris.logothetis@cognitivplus.com>
2023-11-15 16:34:10 +0200

    re-usable Intro comp w/ named slots

--- src/routes/ancient-theatres/+page.svelte
+++ src/routes/ancient-theatres/+page.svelte
@@ -6,6 +6,12 @@
   import NextSteps from "$lib/components/ancient-theatres/NextSteps.svelte";
   import "$lib/scss/global.scss";
 
+  let introPara = [
+    "As I’m a bit of a history nut, I’ve visited quite a few of the archaeological sites in Greece and the southern Balkans. So when I was thinking of a personal project to test my Figma and responsive design skills, I thought this would be a good fit. Especially as I have my own photographs to use.",
+    "There are over a hundred Greek and then Roman theatres, so I just selected five from the Greek period, but nipped over the border into modern day Albania to show two that are relatively unknown.",
+    "Note: as the aim here was to practice web design, the theatre pages use placeholder or duplicated text."
+  ]
+
   let techStack = [
     { title: 'Stack:', description: 'Vanilla JS - CSS - Git/Github' },
     { title: 'Design:', description: 'Figma - VS Code - Chrome Dev Tools' },
@@ -17,9 +23,9 @@
   <Intro>
     <span slot="heading">Ancient Greek Theatres</span>
     <div slot="intro-para">
-      <p>As I’m a bit of a history nut, I’ve visited quite a few of the archaeological sites in Greece and the southern Balkans. So when I was thinking of a personal project to test my Figma and responsive design skills, I thought this would be a good fit. Especially as I have my own photographs to use.</p>
-      <p>There are over a hundred Greek and then Roman theatres, so I just selected five from the Greek period, but nipped over the border into modern day Albania to show two that are relatively unknown.</p>
-      <p>Note: as the aim here was to practice web design, the theatre pages use placeholder or duplicated text.</p>
+      {#each introPara as para }
+        <p>{para}</p>
+      {/each}
     </div>
 
     <div slot="tech-stack">
