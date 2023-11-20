557241c77f350b13928ee91c1e1580fe8c5ab6e5
CW-Logothetis <chris.logothetis@cognitivplus.com>
2023-11-20 17:50:07 +0200

    refactor process component with @html

--- src/lib/components/project-page/Process.svelte
+++ src/lib/components/project-page/Process.svelte
@@ -1,40 +1,46 @@
 <script>
   import Step from "../UI/Step.svelte";
-
   export let processData;
 
+  // Helper function to determine Process content based on the step index
+  // Note: as the content is static, @html is used, allowing for more readable markdown.
+  // if it was dynamic, Svelte's {#if} ... {:else} in the markdown would allow easy reactivity.
+  function getStepContent(text, colorClass, image, stepIndex) {
+    const isOddStep = (stepIndex + 1) % 2 !== 0;
+
+    if (isOddStep) {
+      return `
+        <div class="step-bubble-l 'bubble-white'">
+           ${getParagraphs(text)}
+        </div>
+        <div class="step-bubble-r ${colorClass}">
+           <img src="/${image}.png" class="process--image" alt="">
+        </div>
+    `;
+    } else return `
+        <div class="step-bubble-l ${colorClass}">
+            <img src="/${image}.png" class="process--image" alt="">
+        </div>
+        <div class="step-bubble-r bubble-white">
+            ${getParagraphs(text)}
+        </div>
+    `;
+  }
+
+  // Helper function to generate paragraphs
+  function getParagraphs(text) {
+    return text.map(p => `<p class="process--para">${p}</p>`).join('');
+  }
 </script>
 
 <section class="process">
-
   <div class="main-grid">
-
     <h2 class="u: text-center | process--heading">Process</h2>
 
-    {#each processData as { text, colorClass, image }, i (i)}
-      <Step text={`Step ${i + 1}`} stepNumber={i + 1} />
+    {#each processData as { text, colorClass, image }, stepIndex (stepIndex)}
+      <Step text={`Step ${stepIndex + 1}`} />
       <div class="flex-grid process--step process--grid">
-
-        {#if (i + 1) % 2 !== 0}
-          <div class="step-bubble-l bubble-white">
-            {#each text as p}
-              <p class="process--para">{p}</p>
-            {/each}
-          </div>
-          <div class="step-bubble-r {colorClass}">
-            <img src={`/${image}.png`} class="process--image" alt="">
-          </div>
-
-        {:else }
-          <div class="step-bubble-l {colorClass}">
-            <img src={`/${image}.png`} class="process--image" alt="">
-          </div>
-          <div class="step-bubble-r bubble-white">
-            {#each text as p}
-              <p class="process--para">{p}</p>
-            {/each}
-          </div>
-        {/if}
+        {@html getStepContent(text, colorClass, image, stepIndex)}
       </div>
     {/each}
   </div>
