85ccecb71e539949d4a4ad4847bb90b397604b61
CW-Logothetis <chris.logothetis@cognitivplus.com>
2023-11-30 11:37:02 +0200

    update openAI project page

--- src/lib/page-text/openai.js
+++ src/lib/page-text/openai.js
@@ -16,3 +16,44 @@ export let introData = {
 };
 
 export let heroImage = "/array_homescreen.png"
+
+export let processData =
+  {
+    "sectionHeader": "Process",
+    "steps": [
+      {
+        "text": [
+          "A simple homepage layout with CSS Grid and Flexbox.",
+          "They make it naturally responsive and there's no need for any media queries.",
+          "The colour scheme is based on the JavaScript logo."
+        ],
+        "colorClass": "bubble-4-blue",
+        "image": "array_responsive"
+      },
+      {
+        "text": [
+          "I prompted chatGPT to write the content for the 37 array method cards.",
+          "A first idea was a table. But then I asked it to convert to an object, which populates the cards.",
+          "Saved me a huge amount of time. Even in the 'old' days of text-davinci."
+        ],
+        "colorClass": "bubble-4-blue",
+        "image": "array_openai"
+      },
+      {
+        "text": [
+          "Set up a dynamic route for the text output page, using SvelteKit's `[parameter]`, passing the array method's ID as a param for the URL.",
+          "Each method will have its own 'page', ready for OpenAI generated summary, syntax and examples."
+        ],
+        "colorClass": "bubble-4-blue",
+        "image": "array_page"
+      },
+      {
+        "text": [
+          "As the OpenAI API key is private the request lives in a +page.server.js file, which is fetched before the page is rendered.",
+          "When the user navigates to this page, SvelteKit's load function outputs the response from OpenAI for the `+page.svelte`"
+        ],
+        "colorClass": "bubble-4-blue",
+        "image": "array_page-server"
+      }
+    ]
+  }
--- src/routes/openai/+page.svelte
+++ src/routes/openai/+page.svelte
@@ -1,19 +1,15 @@
 <script>
     import Intro from "$lib/components/project-page/Intro.svelte";
     import Hero from "$lib/components/project-page/Hero.svelte";
-    import { introData, heroImage } from "$lib/page-text/openai.js";
+    import StepData from "$lib/components/project-page/StepData.svelte";
+    import { introData, heroImage, processData } from "$lib/page-text/openai.js";
     import "$lib/scss/global.scss";
 </script>
 
 <div class="flow">
     <Intro {introData} />
     <Hero {heroImage}/>
-<!--    <Process />-->
+    <StepData data = {processData}/>
 <!--    <Bugs />-->
 <!--    <NextSteps />-->
 </div>
-
-
-<style lang="scss">
-
-</style>
