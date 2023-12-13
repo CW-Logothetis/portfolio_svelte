b2b642be879ce686ff77c55f93a06736c2b66f48
CW-Logothetis <chris.logothetis@cognitivplus.com>
2023-12-13 19:13:55 +0200

    adjust hero 'centering' by adding grid rows

--- src/lib/components/home-page/Hero.svelte
+++ src/lib/components/home-page/Hero.svelte
@@ -19,9 +19,9 @@
 <style lang="scss">
 
   .hero {
-    min-height: calc(100dvh - 50px);
+    min-height: 100dvh;
     display: grid;
-    grid-template-rows: repeat(3, 1fr);
+    grid-template-rows: repeat(6,17%);
     align-items: center;
     overflow: hidden;
   }
@@ -29,7 +29,7 @@
   .name-flexbox {
     margin-top: 0;
     grid-column: 2/-2;
-    grid-row: 1/4;
+    grid-row: 3/5;
     display: flex;
     flex-direction: column;
     text-align: left;
@@ -43,7 +43,7 @@
 
   .tech-flexbox {
     grid-column: 2/-2;
-    grid-row: 1/3;
+    grid-row: 2/4;
     display: flex;
     flex-direction: column;
     align-items: flex-end;
