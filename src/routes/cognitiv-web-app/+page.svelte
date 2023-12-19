2be8cd9191b644a1b7fed3e0a56246e85d49fa5c
CW-Logothetis <chris.logothetis@cognitivplus.com>
2023-12-19 19:59:12 +0200

    add cog plus project

--- src/routes/cognitiv-web-app/+page.svelte
+++ src/routes/cognitiv-web-app/+page.svelte
@@ -2,7 +2,7 @@
     import Intro from "$lib/components/project-page/Intro.svelte";
     import Hero from "$lib/components/project-page/Hero.svelte";
     import StepData from "$lib/components/project-page/StepData.svelte";
-    import { introData, heroImage, processData } from "$lib/pageText/openai.js";
+    import { introData, heroImage, processData } from "$lib/pageText/cognitiv_web-app.js";
     import "$lib/scss/global.scss";
 </script>
 
