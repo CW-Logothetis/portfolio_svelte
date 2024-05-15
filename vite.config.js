5fd909ca26a62b4c708836478fe636c7f5d6eef3
CW-Logothetis <chris.logothetis@cognitivplus.com>
2024-05-15 16:01:40 +0300

    install enhanced img and update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,23 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { enhancedImages } from '@sveltejs/enhanced-img';
+import { defineConfig } from 'vite';
 
-const config = {
-	plugins: [sveltekit()],
+export default defineConfig ({
 
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	},
-
+	plugins: [
+		enhancedImages(),
+		sveltekit()
+	],
 	css: {
 		preprocessorOptions: {
 			scss: {
 				// additionalData: '@use "src/variables.scss" as *;'
 			}
 		}
+	},
+	// Vitest Configuration
+	test: {
+		environment: 'jsdom', // jsdom simulates browser, cf 'node'
+		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
-};
-
-export default config;
+});
