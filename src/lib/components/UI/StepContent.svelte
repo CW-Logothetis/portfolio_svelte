ad859fa70c7855795048fb5dc2eb51ec09bfa3bf
CW-Logothetis <chris.logothetis@cognitivplus.com>
2024-05-16 20:02:05 +0300

    new comps for project page enhanced img

---
+++ src/lib/components/UI/StepContent.svelte
@@ -0,0 +1,28 @@
+<script>
+  export let text;
+  export let colorClass;
+  export let image;
+  export let alt;
+  export let isOddStep;
+
+  // generate paragraphs
+  function getParagraphs(text) {
+    return text.map(p => `<p class="c: process--para">${p}</p>`).join('');
+  }
+</script>
+
+{#if isOddStep}
+    <div class="c: step-bubble-l 'bubble-white'">
+        {@html getParagraphs(text)}
+    </div>
+    <div class="c: step-bubble-r {colorClass}">
+        <enhanced:img src="{image}?w=1280;640;400" sizes="(min-width:1920px) 1280px, (min-width:1080px) 640px, (min-width:768px) 400px" class="process--image" alt="{alt}" />
+    </div>
+{:else}
+    <div class="c: step-bubble-l {colorClass}">
+        <enhanced:img src="{image}?w=1280;640;400" sizes="(min-width:1920px) 1280px, (min-width:1080px) 640px, (min-width:768px) 400px" class="process--image" alt="{alt}" />
+    </div>
+    <div class="c: step-bubble-r bubble-white">
+        {@html getParagraphs(text)}
+    </div>
+{/if}
--- src/lib/components/project-page/StepData.svelte
+++ src/lib/components/project-page/StepData.svelte
@@ -1,45 +1,18 @@
 <script>
   import StepCounter from "../UI/StepCounter.svelte";
+  import StepContent from "../UI/StepContent.svelte";
   export let data;
 
-  // Helper function to determine Process content based on the step index
-  // Note: as the content is static, @html is used, allowing for more readable markdown.
-  // if it was dynamic, Svelte's {#if} ... {:else} in the markdown would allow easy reactivity.
-  function getStepContent(text, colorClass, image, stepIndex) {
-    const isOddStep = (stepIndex + 1) % 2 !== 0;
-    if (isOddStep) {
-      return `
-        <div class="c: step-bubble-l 'bubble-white'">
-           ${getParagraphs(text)}
-        </div>
-        <div class="c: step-bubble-r ${colorClass}">
-           <img src="/${image}.png" class="process--image" alt="">
-        </div>
-    `;
-    } else return `
-        <div class="c: step-bubble-l ${colorClass}">
-            <img src="/${image}.png" class="process--image" alt="">
-        </div>
-        <div class="c: step-bubble-r bubble-white">
-            ${getParagraphs(text)}
-        </div>
-    `;
-  }
-
-  // Helper function to generate paragraphs
-  function getParagraphs(text) {
-    return text.map(p => `<p class="c: process--para">${p}</p>`).join('');
-  }
 </script>
 
 <section class="e: process">
   <div class="c: main-grid">
     <h2 class="u: text-center | e: process--heading">{data.sectionHeader}</h2>
 
-    {#each data.steps as { text, colorClass, image }, stepIndex (stepIndex)}
+    {#each data.steps as { text, colorClass, image, alt }, stepIndex (stepIndex)}
       <StepCounter text={`Step ${stepIndex + 1}`} />
       <div class="c: process--step">
-        {@html getStepContent(text, colorClass, image, stepIndex)}
+        <StepContent {text} {colorClass} {image} {alt} isOddStep={(stepIndex + 1) % 2 !== 0} />
       </div>
     {/each}
   </div>
